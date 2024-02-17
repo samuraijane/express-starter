a46dda4810a66b1b4c01e3f904e9bb2bfacd3384
Matthew Day <matt@matthewday.net>
2024-02-17 08:13:59 -0500

    configure server with /heartbeat endpoint

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("server tbd");
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const server = express();
+
+const { PORT } = process.env;
+
+server.get('/heartbeat', (req, res) => {
+  res.json({
+    "is": "working"
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`The server is listening at port ${PORT}`);
+});
